851e25abacdaed9bfb02b57cd566f3a6c474a613
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2023-07-21 12:56:18 +0500

    update

--- pages/manageSystem.php
+++ pages/manageSystem.php
@@ -1,21 +1,56 @@
-<form action="update_record.php" method="POST">
-    <input type="hidden" name="id" value="значение_id_записи_для_изменения">
+<?php
+$id = $_GET['update'];
+$title = "";
+$owner_id = -1;
 
-    <label for="title">Заголовок:</label>
-    <input type="text" name="title"="title" value="текущее_значение_заголовка">
 
-    <label for="owner">Владелец:</label>
-    <select>
-        <?php
-        $sql = "SELECT id, FIO FROM Owners";
-        if ($result = $conn->query($sql)) {
-            // Генерация опций выпадающего списка
-            foreach ($result as $owner) {
-                echo '<option value="' . $owner['id'] . '">' . $owner['FIO'] . '</option>';
+$sql = "SELECT * FROM InformSystems where id = " . $id;
+
+
+if ($result = $conn->query($sql)) {
+    foreach ($result as $owner) {
+        $title = $owner['title'];
+        $owner_id = $owner['owner'];
+    }
+}
+
+
+?>
+<div class="login-form update-form">
+    <form action="" method="POST">
+        <input type="hidden" name="id" value="">
+
+        <label for="title">Название информационной системы:</label>
+        <input type="text" name="title" value="<?php echo $title ?>">
+
+        <label for="owner">Владелец:</label>
+        <select name="owner">
+            <?php
+
+            $sql = "SELECT id, FIO FROM Owners";
+            if ($result = $conn->query($sql)) {
+                // Генерация опций выпадающего списка
+                foreach ($result as $owner) {
+                    if ((int) $owner['id'] === (int) $owner) {
+                        echo '<option selected="selected" value="' . $owner['id'] . '">' . $owner['FIO'] . '</option>';
+                    } else {
+                        echo '<option value="' . $owner['id'] . '">' . $owner['FIO'] . '</option>';
+                    }
+                }
             }
-        }
-        ?>
-    </select>
+            ?>
+        </select>
 
-    <input type="submit" value="Изменить">
-</form>
+        <input type="submit" value="Изменить">
+    </form>
+</div>
+
+<?php
+if (isset($_POST['title'])) {
+    if (isset($_POST['owner'])) {
+        $sql = "update InformSystems set title='" . $_POST['title'] . "', owner=" . $_POST['owner'] . ' where id=' . $id;
+        $result = $conn->query($sql);
+        header('Location: ' . 'index.php');
+    }
+}
+?>
